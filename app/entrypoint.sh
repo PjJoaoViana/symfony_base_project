17496cb7577843d9b03fa6a97700a93b77ae7b04
PjJoaoViana <joao.viana.it@gmail.com>
2023-02-12 21:12:59 +0000

    cenas

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-apt install -y
-composer install
-symfony console --no-interaction doctrine:migrations:migrate
-apt install npm -y
-npm install -D tailwindcss postcss autoprefixer postcss-loader
+docker exec php apt install -y
+docker exec php apt install npm
+docker exec php composer install
+docker exec php symfony console --no-interaction doctrine:migrations:migrate
+docker exec php apt install npm -y
+#docker exec php curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+#docker exec php nvm install --latest-npm
